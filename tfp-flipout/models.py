5f686f4c07ca764be2b4466522f5a433c68e1e0b
Daigo HIROOKA <Daigo@Daigo-no-MacBook-Air.local>
2020-04-01 01:32:17 +0900

    create model functions #3

---
+++ tfp-flipout/models.py
@@ -0,0 +1,45 @@
+''' 3 types of CNN (normal/reparameterized/flipout) '''
+
+import tensorflow as tf
+import tensorflow_probability as tfp
+from tensorflow.keras import layers
+
+
+def CNN(num_classes=10, **kwargs):
+    model = tf.keras.Sequential([
+        layers.Conv2D(64, 3, 2, 'same', activation=tf.nn.relu),
+        layers.Conv2D(64, 3, 1, 'same', activation=tf.nn.relu),
+        layers.Conv2D(128, 3, 2, 'same', activation=tf.nn.relu),
+        layers.Conv2D(128, 3, 1, 'same', activation=tf.nn.relu),
+        layers.Conv2D(num_classes, 3, 1, 'same'),
+        layers.GlobalAvgPool2D()
+    ], **kwargs)
+    return model
+
+
+def ReparamCNN(num_classes, **kwargs):
+    Conv2DReparam = tfp.layers.Convolution2DReparameterization
+
+    model = tf.keras.Sequential([
+        Conv2DReparam(64, 3, 2, 'same', activation=tf.nn.relu),
+        Conv2DReparam(64, 3, 1, 'same', activation=tf.nn.relu),
+        Conv2DReparam(128, 3, 2, 'same', activation=tf.nn.relu),
+        Conv2DReparam(128, 3, 2, 'same', activation=tf.nn.relu),
+        Conv2DReparam(num_classes, 3, 1, 'same'),
+        layers.GlobalAvgPool2D()
+    ], **kwargs)
+    return model
+
+
+def FlipOutCNN(num_classes, **kwargs):
+    Conv2DFlipOut = tfp.layers.Convolution2DFlipout
+
+    model = tf.keras.Sequential([
+        Conv2DFlipOut(64, 3, 2, 'same', activation=tf.nn.relu),
+        Conv2DFlipOut(64, 3, 1, 'same', activation=tf.nn.relu),
+        Conv2DFlipOut(128, 3, 2, 'same', activation=tf.nn.relu),
+        Conv2DFlipOut(128, 3, 2, 'same', activation=tf.nn.relu),
+        Conv2DFlipOut(num_classes, 3, 1, 'same'),
+        layers.GlobalAvgPool2D()
+    ], **kwargs)
+    return model
