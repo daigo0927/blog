a64b3a7ba8bac1d28ed50e0b6d7b836c7a7d5ff8
Daigo Hirooka <daigo.hirooka@gmail.com>
2021-06-19 08:44:00 +0900

    retrieve completed results

--- streamlit-fastapi-example/streamlit-front/app.py
+++ streamlit-fastapi-example/streamlit-front/app.py
@@ -6,15 +6,22 @@ import streamlit as st
 BACKEND_HOST = os.environ.get('BACKEND_HOST', '127.0.0.1:80')
 
 
+@st.cache
+def retrieve_results(job_id: str = None) -> httpx.Response:
+    params = {'job_id': job_id} if job_id else None
+    r = httpx.get(f'http://{BACKEND_HOST}/results', params=params)
+    return r
+
+
 image_files = st.file_uploader('Target image file',
                                type=['png', 'jpg'],
                                accept_multiple_files=True)
 
-if len(image_files) > 0:
+if len(image_files) > 0 and st.button('submit'):
     files = [('files', file) for file in image_files]
 
     r = httpx.post(f'http://{BACKEND_HOST}/predict', files=files)
-    st.write(r)
+    st.success(r.json())
 
-# r = httpx.get(f'http://{BACKEND_HOST}/result')
-# result_jobs = r.json()['result_jobs']
+r = retrieve_results()
+st.write(r.json())
