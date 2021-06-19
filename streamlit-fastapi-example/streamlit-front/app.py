b4304f15d299f09ab72a1e4e40a67715fef28837
Daigo Hirooka <daigo.hirooka@gmail.com>
2021-06-19 19:38:54 +0900

    show processed list

--- streamlit-fastapi-example/streamlit-front/app.py
+++ streamlit-fastapi-example/streamlit-front/app.py
@@ -1,27 +1,36 @@
 import os
 import httpx
 import streamlit as st
+import pandas as pd
+from typing import List
+
+
+def format_results(result_files: List[str]) -> pd.DataFrame:
+    job_indices, filenames = [], []
+    for _, job_id, filename in map(lambda s: s.split('/'), result_files):
+        job_indices.append(job_id)
+        filenames.append(filename)
+    df = pd.DataFrame({'job_id': job_indices, 'filename': filenames})
+    return df
 
 
 BACKEND_HOST = os.environ.get('BACKEND_HOST', '127.0.0.1:80')
-
-
-@st.cache
-def retrieve_results(job_id: str = None) -> httpx.Response:
-    params = {'job_id': job_id} if job_id else None
-    r = httpx.get(f'http://{BACKEND_HOST}/results', params=params)
-    return r
 
 
 image_files = st.file_uploader('Target image file',
                                type=['png', 'jpg'],
                                accept_multiple_files=True)
 
-if len(image_files) > 0 and st.button('submit'):
+if len(image_files) > 0 and st.button('Submit'):
     files = [('files', file) for file in image_files]
 
     r = httpx.post(f'http://{BACKEND_HOST}/predict', files=files)
     st.success(r.json())
 
-r = retrieve_results()
-st.write(r.json())
+
+if st.button('Refresh'):
+    st.success('Refreshed')
+    
+r = httpx.get(f'http://{BACKEND_HOST}/results')
+df_results = format_results(r.json())
+st.write(df_results)
