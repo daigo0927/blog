f9b36180ebf6597431802f82a2b8838526e44368
Daigo Hirooka <daigo.hirooka@gmail.com>
2021-08-15 02:18:49 +0900

    change job-dir arg to output-dir

--- ml-workflow/preprocess/main.py
+++ ml-workflow/preprocess/main.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import StratifiedKFold
 SEED = 42
 
 
-def run(job_dir: str, csv_path: str, n_splits: int) -> None:
+def run(csv_path: str, output_dir: str, n_splits: int) -> None:
     df = pd.read_csv(csv_path)
     print(f"Load CSV from: {csv_path}")
 
@@ -18,17 +18,17 @@ def run(job_dir: str, csv_path: str, n_splits: int) -> None:
     df_train = df.iloc[idx_train]
     df_val = df.iloc[idx_val]
 
-    df_train.to_csv(f'{job_dir}/train.csv', index=False)
-    df_val.to_csv(f'{job_dir}/val.csv', index=False)
-    print(f'Save train/val data to: {job_dir}')
+    df_train.to_csv(f'{output_dir}/train.csv', index=False)
+    df_val.to_csv(f'{output_dir}/val.csv', index=False)
+    print(f'Save train/val data to: {output_dir}')
 
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preprocess')
-    parser.add_argument('--job-dir', type=str, required=True,
-                        help='GCS location for writing outputs')
     parser.add_argument('--csv-path', type=str, required=True,
                         help='Source CSV file location')
+    parser.add_argument('--output-dir', type=str, required=True,
+                        help='GCS location for writing outputs')    
     parser.add_argument('--n-splits', type=int, default=3,
                         help='Number of train/val splits. [3] as default')
     args = parser.parse_args()
