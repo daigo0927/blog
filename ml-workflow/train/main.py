b2ec16c530bb79772422d3767bcac5e8e1b4312c
Daigo Hirooka <daigo.hirooka@gmail.com>
2021-08-15 19:03:44 +0900

    add a train component

---
+++ ml-workflow/train/main.py
@@ -0,0 +1,83 @@
+import argparse
+import pandas as pd
+import lightgbm as lgb
+from google.cloud import storage
+from sklearn.metrics import accuracy_score
+
+
+SEED = 42
+
+
+def run(datadir: str,
+        out_bucket: str,
+        learning_rate: float,
+        max_depth: int,
+        bagging_fraction: float,
+        feature_fraction: float,
+        lambda_l1: float,
+        lambda_l2: float,
+        min_data_in_leaf: int,
+        num_leaves: int) -> None:
+    df_train = pd.read_csv(f'{datadir}/train.csv')
+    df_val = pd.read_csv(f'{datadir}/val.csv')
+    print(f'Data size: train: {df_train.shape}, val: {df_val.shape}')
+
+    x_train, y_train = df_train.drop(['target'], axis=1), df_train['target']
+    x_val, y_val = df_val.drop(['target'], axis=1), df_val['target']
+
+    ds_train = lgb.Dataset(x_train, label=y_train)
+    ds_val = lgb.Dataset(x_val, label=y_val)
+
+    params = {
+        'objective': 'multiclass',
+        'num_class': 3,
+        'learning_rate': learning_rate,
+        'max_depth': max_depth,
+        'bagging_fraction': bagging_fraction,
+        'feature_fraction': feature_fraction,
+        'lambda_l1': lambda_l1,
+        'lambda_l2': lambda_l2,
+        'min_data_in_leaf': min_data_in_leaf,
+        'num_leaves': num_leaves,
+        'random_state': SEED,
+        'verbose': -1
+    }
+
+    model = lgb.train(params,
+                      ds_train,
+                      num_boost_round=1000,
+                      early_stopping_rounds=10,
+                      valid_sets=[ds_train, ds_val],
+                      verbose_eval=50)
+
+    y_pred = model.predict(x_val, num_iteration=model.best_iteration)
+    y_pred = y_pred.argmax(axis=-1)
+    acc_val = accuracy_score(y_val, y_pred)
+    print(f'Validation accuracy: {acc_val}')
+
+    savefile = 'model.lgb'
+    model.save_model(savefile)
+
+    bucket = storage.Client().bucket(out_bucket)
+    blob = bucket.blob(savefile)
+    blob.upload_from_filename(savefile)
+    print(f'Upload model to: gs://{out_bucket}/{savefile}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train')
+    parser.add_argument('--datadir', type=str, required=True,
+                        help='GCS location for train/val data')
+    parser.add_argument('--out-bucket', type=str, required=True,
+                        help='GCS location for outputs')
+    parser.add_argument('--learning-rate', type=float, default=0.1)
+    parser.add_argument('--max-depth', type=int, default=10)
+    parser.add_argument('--bagging-fraction', type=float, default=0.7)
+    parser.add_argument('--feature-fraction', type=float, default=0.7)
+    parser.add_argument('--lambda_l1', type=float, default=1.0)
+    parser.add_argument('--lambda_l2', type=float, default=1.0)
+    parser.add_argument('--min-data-in-leaf', type=int, default=10)
+    parser.add_argument('--num-leaves', type=int, default=40)
+
+    args = parser.parse_args()
+    run(**vars(args))
